36ef94ff3aa917c4f5435c49328ecd7785908698
Minyoungkyu <alsdudrb123@gmail.com>
2024-04-04 17:10:49 +0900

    set pyodide2

--- front/src/lib/pyodide/pyodide.ts
+++ front/src/lib/pyodide/pyodide.ts
@@ -26,7 +26,7 @@ let pyodideWorker:any = null;
 
 function getPyodideWorker() {
     if (pyodideWorker === null) {
-        pyodideWorker = new Worker('/pyodideWorkers.js');
+        pyodideWorker = new Worker('/pyodideWorker.js');
 
         pyodideWorker.onmessage = (event:any) => {
             if (pyodideWorker.callback) {
